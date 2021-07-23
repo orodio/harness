fee06ed8ff3f51222f38da31b12479ea272ef1d4
Greg Santos <greg.santos@dapperlabs.com>
2021-07-23 14:01:12 -0700

    Add new view config

--- src/config.js
+++ src/config.js
@@ -8,17 +8,20 @@ fcl.config()
   // .put("app.detail.icon", "https://i.imgur.com/r23Zhvu.png")
   .put("app.detail.icon", "https://placekitten.com/g/200/200")
   .put("service.OpenID.scopes", "email")
+// .put("serviceStrategy", "frame")
 
 if (USE_LOCAL) {
   // prettier-ignore
   fcl.config()
     .put("env", "local")
     .put("accessNode.api", "http://localhost:8080")
-    .put("discovery.wallet", "http://localhost:3000/fcl/authn")
+    .put("challenge.handshake", "http://localhost:8701/fcl/authn")
+    .put("discovery.wallet.view", "frame")
 } else {
   // prettier-ignore
   fcl.config()
     .put("env", "testnet")
     .put("accessNode.api", "https://access-testnet.onflow.org")
-    .put("discovery.wallet", "https://fcl-discovery.onflow.org/testnet/authn")
+    .put("challenge.handshake", "https://fcl-discovery.onflow.org/testnet/authn")
+    .put("discovery.wallet.view", "frame")
 }
